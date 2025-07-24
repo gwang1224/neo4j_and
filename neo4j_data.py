7a61aac2622e8df9f2d63fdaaf2ced30358a2b32
gwang1224 <gracewang187@gmail.com>
2025-07-23 20:30:35 -0700

    publication node

--- neo4j_data.py
+++ neo4j_data.py
@@ -10,17 +10,16 @@ author_id_to_label = {}
 for i, author_id in enumerate(author_data.keys()):
     author_id_to_label[author_id] = str(i)
 
-# 2. Fetch works for each author
+#3. Fetch works for each author
 works_data = {}
 for author_id, author in author_data.items():
     author_works = oth.fetch_works_for_author(author_id)
     author["works"] = [w["id"] for w in author_works]
 
+    for work in author_works:
+            works_data[work["id"]] = work
 
-#3. Save to JSON
+#4. Save to JSON
 oth.save_data_to_json(author_name, author_data, works_data, author_id_to_label)
-
-
-
 
 
--- neo4j_import.py
+++ neo4j_import.py
@@ -1,23 +1,57 @@
 from neo4j import GraphDatabase
+from neo4j.exceptions import ServiceUnavailable
+import json
 
 
 # Importing data into Neo4j, ref: https://neo4j.com/docs/python-manual/current/
 URI = "neo4j://127.0.0.1:7687"
 AUTH = ("neo4j", "and123$$")
 
-with GraphDatabase.driver(URI, auth=AUTH) as driver:
-    driver.verify_connectivity()
+try:
+    with GraphDatabase.driver(URI, auth=AUTH) as driver:
+        driver.verify_connectivity()
+        print("Connection to Neo4j database successful!")
+except ServiceUnavailable as e:
+    print(f"Connection failed: {e}")
 
-summary = driver.execute_query("""
-    CREATE (a:Person {name: $name})
-    CREATE (b:Person {name: $friendName})
-    CREATE (a) - [:LIKES] -> (b)                          
-    """,
-    name = "Grace", friendName="Leo",
-    database_="neo4j",
-).summary
-print("Created {nodes_created} nodes in {time} ms.".format(
-    nodes_created=summary.counters.nodes_created,
-    time=summary.result_available_after
-))
 
+def publication_as_nodes(driver, json_path):
+    """
+    Adds each publication as a node with metadata id, title, year, authors
+
+    Args
+        driver (Object): neo4j
+        json_path (str): OpenAlex data... see cache
+    """
+    with open(json_path,'r') as file:
+        data = json.load(file)
+
+    for work in data['works_data']:
+        id, title, year, authors = (
+            data['works_data'][work][k] for k in ['id', 'title', 'year', 'authors']
+        )
+
+        driver.execute_query("""
+            CREATE (n:PUBLICATION {id: $pub_id, title: $pub_title, year: $pub_year})
+            """,
+            pub_id = id, pub_title = title, pub_year = year,
+            database = "neo4j",
+        )
+    
+
+publication_as_nodes(driver, "/Users/gracewang/Documents/UROP_Summer_2025/neo4j_and/cache/Russell Bowler_data.json")
+
+#def delete_all_nodes(driver):
+
+# summary = driver.execute_query("""
+#     CREATE (a:Person {name: $name})
+#     CREATE (b:Person {name: $friendName})
+#     CREATE (a) - [:LIKES] -> (b)                          
+#     """,
+#     name = "Grace", friendName="Leo",
+#     database_="neo4j",
+# ).summary
+# print("Created {nodes_created} nodes in {time} ms.".format(
+#     nodes_created=summary.counters.nodes_created,
+#     time=summary.result_available_after
+# ))
